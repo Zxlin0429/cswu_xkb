fbd7be1416c713c3d30c807ecf5958723ea03fb1
Zxlin <970514711@qq.com>
2020-12-23 23:25:46 +0800

    校快办 1.0

--- src/App.vue
+++ src/App.vue
@@ -21,15 +21,14 @@ export default {
   },
   mounted() {
     //禁止使用微信以外打开
-  // var useragent = navigator.userAgent;
-  // if (useragent.match(/MicroMessenger/i) != "MicroMessenger") {
-  //   // 这里警告框会阻塞当前页面继续加载
-  //   alert("已禁止本次访问：您必须使用微信内置浏览器访问本页面！");
-  //   // 以下代码是用javascript强行关闭当前页面
-  //   var opened = window.open("提示", "_self");
-  //   opened.opener = null;
-  //   opened.close();
-  // }
+    // var ua = navigator.userAgent.toLowerCase();
+    // var isWeixin = ua.indexOf("micromessenger") != -1;
+    // if (!isWeixin) {
+    //   document.head.innerHTML =
+    //     '<title>抱歉，出错了</title><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0"><link rel="stylesheet" type="text/css" href="https://res.wx.qq.com/open/libs/weui/0.4.1/weui.css">';
+    //   document.body.innerHTML =
+    //     '<div class="weui_msg"><div class="weui_icon_area"><i class="weui_icon_info weui_icon_msg"></i></div><div class="weui_text_area"><h4 class="weui_msg_title">请在微信客户端打开链接</h4></div></div>';
+    // }
   },
   methods: {
     activityScroll() {
